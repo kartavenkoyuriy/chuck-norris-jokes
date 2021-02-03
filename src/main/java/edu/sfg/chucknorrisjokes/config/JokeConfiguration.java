1a76f6c2af05be710f3a92964b387cb23857ab16
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2021-02-03 07:20:07 +0200

    refactored, move dependency

---
+++ src/main/java/edu/sfg/chucknorrisjokes/config/JokeConfiguration.java
@@ -0,0 +1,14 @@
+package edu.sfg.chucknorrisjokes.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes getChuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/edu/sfg/chucknorrisjokes/services/JokeService.java
+++ src/main/java/edu/sfg/chucknorrisjokes/services/JokeService.java
@@ -4,9 +4,13 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ChuckNorrisJokesService {
+public class JokeService {
 
-    ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeService(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
 
     public String getJoke(){
         return chuckNorrisQuotes.getRandomQuote();
